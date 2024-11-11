577d8a443055714615f83364e9737caa4aeaa76c
Nick Arini <nick@generative.vision>
2024-11-11 16:27:01 +0000

    Update installer script

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,18 @@
-def cli():
-    print("Hello from the CLI!")
+import typer
+
+
+app = typer.Typer()
+
+
+@app.command()
+def init():
+    print("Initiating new Fabric project")
+
+
+@app.command()
+def run():
+    print("Starting Fabric server")
 
 
 if __name__ == "__main__":
-    cli()
+    app()
